2bb51838876d56e582d36c4fd7e4d9d76ce79804
ElenaK <kinder.bubnova@yandex.ru>
2022-07-06 10:32:50 +0500

    JS done

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+
+const popupElement = document.querySelector('.popup');
+
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__edit');
+
+const nameElement = document.querySelector('.profile__title');
+const popupNameElement = document.querySelector('.popup__name');
+
+const jobElement = document.querySelector('.profile__subtitle');
+const popupJobElement = document.querySelector('.popup__profession');
+
+
+const showPopup = function() {
+    popupNameElement.value = nameElement.textContent;
+    popupElement.classList.add('popup_opened');
+
+    popupJobElement.value = jobElement.textContent;
+    popupElement.classList.add('popup_opened');
+};
+
+const closePopup = function() {
+    popupElement.classList.remove('popup_opened');
+};
+
+const applyPopup = function() {
+    nameElement.textContent = popupNameElement.value;
+    closePopup();
+
+    jobElement.textContent = popupJobElement.value;
+    closePopup();
+};
+
+popupOpenButtonElement.addEventListener('click', showPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+
+let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+
+    applyPopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
